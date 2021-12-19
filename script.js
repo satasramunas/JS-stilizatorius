870048bc8b8155c928bfe1df19cfae6536559f99
Ramunas <ramunas.satas@outlook.com>
2021-12-19 13:03:33 +0200

    added height, width

--- script.js
+++ script.js
@@ -2,4 +2,15 @@ function myFunction() {
     let myText = document.getElementById("content").value;
     document.getElementById("box").innerHTML = "" + myText;
 
+    let myBox = document.getElementById("box").style;
+
+    let textSize = document.getElementById("size").value;
+    let textHeight = document.getElementById("height").value;
+    let textWidth = document.getElementById("width").value
+    
+    myBox.fontSize = textSize + "px";
+    myBox.height = textHeight + "px";
+    myBox.width = textWidth + "px";
+
+
 }
